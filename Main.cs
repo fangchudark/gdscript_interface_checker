7d3321002a7f3716e096ee83ee65c19269134ebe
fangchudark <158054032+fangchudark@users.noreply.github.com>
2025-12-14 11:25:32 +0800

    Add GDScript interface definitions and check examples

--- Main.cs
+++ Main.cs
@@ -7,54 +7,15 @@ public partial class Main : Control
 	public override void _Ready()
 	{
 		var script = GD.Load<GDScript>("res://example/enemy.gd");
-		/*
-		List<GDScriptMethodInfo> @interface = [
-			// take_damage(int damage) -> void
-			new GDScriptMethodInfo(
-				Name: "take_damage",
-				Args: [
-					new GDScriptPropertyInfo(
-						Name: "damage", // Interface checks will not compare parameter names
-						ClassName: "",
-						Type: Variant.Type.Int,
-						Hint: PropertyHint.None,
-						HintString: "",
-						Usage: PropertyUsageFlags.None
-					)
-				],
-				DefaultArgs: [],
-				Flags: MethodFlags.Normal,
-				Return: new GDScriptPropertyInfo(
-					Name: "",
-					ClassName: "",
-					Type: Variant.Type.Nil,
-					Hint: PropertyHint.None,
-					HintString: "",
-					Usage: PropertyUsageFlags.Default
-				)
-			),
-
-			// property getter
-			new GDScriptMethodInfo(
-				Name: "@is_dead_getter",
-				Args: [],
-				DefaultArgs: [],
-				Flags: MethodFlags.Normal,
-				Return: new GDScriptPropertyInfo(
-					Name: "",
-					ClassName: "",
-					Type: Variant.Type.Bool,
-					Hint: PropertyHint.None,
-					HintString: "",
-					Usage: PropertyUsageFlags.None
-				)
-			)
-		];
-		*/
-
 		// For C#, you can use source generators to generate GDScriptMethodInfo
 		List<GDScriptMethodInfo> @interface = [IDamageableGDScriptMapping.IsDeadGetter, IDamageableGDScriptMapping.TakeDamage];
-		GD.Print(InterfaceChecker.IsImplementedInterface(script,@interface));
+		GD.Print("GDScript implemented C# interface? : ", InterfaceChecker.IsImplementedInterface(script, @interface));
+
+		var interfaceGDScript = GD.Load<GDScript>("res://example/idamageable.gd");
+		var gdScriptInterface = InterfaceChecker.ConstructInterfaceFromScript(interfaceGDScript);
+		GD.Print("GDScript implemented GDScript interface? : ", InterfaceChecker.IsImplementedInterface(interfaceGDScript, gdScriptInterface));
+		
+
 	}
 
 }
---
+++ example/idamageable.gd
@@ -0,0 +1,9 @@
+@abstract
+class_name IDamageable
+
+var is_dead:
+    get:
+        pass
+
+func tack_damage(_damage:int):
+    pass
